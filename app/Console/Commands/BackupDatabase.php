bb6b6d4a88dcdd0be4fcc57f7d135b5ae77f9db9
Leslie Joe <leslie.kojo.okyere@outlook.com>
2022-11-09 08:12:48 +0000

    add

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class BackupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        info("Running the job to backup the database at " . now());
+
+        return Command::SUCCESS;
+    }
+}
